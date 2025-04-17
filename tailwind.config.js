b2382d2b57c71c4ab8957cf133f7a5eeb6bf3267
Alain Mani <requiemcreatif@gmail.com>
2025-04-17 21:21:53 +0200

    initial settings, tailwind setup, shadcn components installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        "ping-slow": "ping 2s cubic-bezier(0, 0, 0.2, 1) infinite",
+      },
+    },
+  },
   plugins: [require("tw-animate-css")],
 };
